f224e30099ed342c1cde423ae0da2bfb5b988edd
limchaehwan <chlim20@naver.com>
2024-12-08 01:57:15 +0900

    GET 방식으로 로그아웃 구현

--- src/main/java/com/example/security/users/controller/LoginController.java
+++ src/main/java/com/example/security/users/controller/LoginController.java
@@ -2,7 +2,13 @@ package com.example.security.users.controller;
 
 
 import com.example.security.users.domain.Role;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,4 +27,17 @@ public class LoginController {
         model.addAttribute("roles", Role.values()); // Enum 값을 모델에 추가
         return "login/signup"; // signup.html로 이동
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        //인증된 사용자가 로그인한 상태 즉 널이아니면
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+
+        return "redirect:/";
+    }
+
 }
